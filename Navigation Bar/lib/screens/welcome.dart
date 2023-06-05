042faca548d106899911fd46b81d35bb130ad090
ToBaEs <128121420+ToBaEs@users.noreply.github.com>
2023-06-05 18:59:35 +0300

    Add files via upload

---
+++ Navigation Bar/lib/screens/welcome.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:project/utils/mydrawer.dart';
+
+class Welcome extends StatefulWidget {
+  const Welcome({super.key});
+
+  @override
+  State<Welcome> createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  int _selectedIndex = 0;  
+  static List<Widget> _widgetOptions = <Widget>[
+    Text('Ana Sayfa'),
+    Text('Kurumsal'),
+    Text('İletişim'),
+    Text('Ayarlar'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: BottomNavigationBarExample(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class BottomNavigationBarExample extends StatefulWidget {
+  const BottomNavigationBarExample({super.key});
+
+  @override
+  State<BottomNavigationBarExample> createState() =>
+      _BottomNavigationBarExampleState();
+}
+
+class _BottomNavigationBarExampleState
+    extends State<BottomNavigationBarExample> {
+  int _selectedIndex = 0;
+  static const TextStyle optionStyle =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+  static const List<Widget> _widgetOptions = <Widget>[
+    Text(
+      'Anasayfa',
+      style: optionStyle,
+    ),
+    Text(
+      'Kurumsal',
+      style: optionStyle,
+    ),
+    Text(
+      'İletişim',
+      style: optionStyle,
+    ),
+    Text(
+      'Ayarlar',
+      style: optionStyle,
+    ),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bottom Bar'),
+      ),
+      drawer: const MyDrawer(),
+      body: Center(
+        child: _widgetOptions.elementAt(_selectedIndex),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Anasayfa',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.business),
+            label: 'Kurumsal',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.school),
+            label: 'İletişim',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Ayarlar',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
